471574e2eeb17d28f263651398f8de9f69d6b0c1
Jean-Christophe Helary <jean.christophe.helary@traduction-libre.org>
2019-12-15 23:48:01 +0900

    add escapes for html atributes...

--- adventuresintechland_old.el
+++ adventuresintechland_old.el
@@ -46,11 +46,11 @@
 ))
   (setq todayTemplate
         (format "<html>
-    <head lang="en-us">
+    <head lang=\"en-us\">
         <title>%1$s</title>
         <link rel=\"stylesheet\" type=\"text/css\" href=\"%2$s\" class=\"baseCSS\"/>
         <link rel=\"stylesheet\" type=\"text/css\" href=\"%3$s\" class=\"dailyCSS\"/>
-        <meta charset="UTF-8" />
+        <meta charset=\"UTF-8\" />
     </head>
     <body>
         %6$s
