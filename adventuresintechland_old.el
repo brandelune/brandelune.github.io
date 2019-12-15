c2b1b774c938ad974a5079da714276ced88b0129
Jean-Christophe Helary <jean.christophe.helary@traduction-libre.org>
2019-12-15 12:51:21 +0900

    add language and charset info to the template

--- adventuresintechland_old.el
+++ adventuresintechland_old.el
@@ -46,10 +46,11 @@
 ))
   (setq todayTemplate
         (format "<html>
-    <head>
+    <head lang="en-us">
         <title>%1$s</title>
         <link rel=\"stylesheet\" type=\"text/css\" href=\"%2$s\" class=\"baseCSS\"/>
         <link rel=\"stylesheet\" type=\"text/css\" href=\"%3$s\" class=\"dailyCSS\"/>
+        <meta charset="UTF-8" />
     </head>
     <body>
         %6$s
