2a1b096e057eae29b00c2bd7fd24cd83bee9a7b7
Jean-Christophe Helary <jean.christophe.helary@traduction-libre.org>
2020-01-03 03:13:56 +0900

    add favicon code to the template

--- adventuresintechland.el
+++ adventuresintechland.el
@@ -103,6 +103,29 @@ the date should be comprised between 1 and (28 to 31)"
         (format "<html>
     <head lang=\"en-us\">
     <title>%1$s</title>
+	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
+	<meta name=\"msapplication-TileColor\" content=\"#FFFFFF\" />
+	<meta name=\"msapplication-TileImage\" content=\"https://brandelune.github.io/favicon/favicon-144.png\" />
+	<meta name=\"msapplication-config\" content=\"https://brandelune.github.io/favicon/browserconfig.xml\" />
+
+	<link rel=\"shortcut icon\" href=\"https://brandelune.github.io/favicon/favicon.ico\" />
+	<link rel=\"icon\" sizes=\"16x16 32x32 64x64\" href=\"https://brandelune.github.io/favicon/favicon.ico\" />
+	<link rel=\"icon\" type=\"image/png\" sizes=\"196x196\" href=\"https://brandelune.github.io/favicon/favicon-192.png\" />
+	<link rel=\"icon\" type=\"image/png\" sizes=\"160x160\" href=\"https://brandelune.github.io/favicon/favicon-160.png\" />
+	<link rel=\"icon\" type=\"image/png\" sizes=\"96x96\" href=\"https://brandelune.github.io/favicon/favicon-96.png\" />
+	<link rel=\"icon\" type=\"image/png\" sizes=\"64x64\" href=\"https://brandelune.github.io/favicon/favicon-64.png\" />
+	<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"https://brandelune.github.io/favicon/favicon-32.png\" />
+	<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"https://brandelune.github.io/favicon/favicon-16.png\" />
+	<link rel=\"apple-touch-icon\" href=\"https://brandelune.github.io/favicon/favicon-57.png\" />
+	<link rel=\"apple-touch-icon\" sizes=\"114x114\" href=\"https://brandelune.github.io/favicon/favicon-114.png\" />
+	<link rel=\"apple-touch-icon\" sizes=\"72x72\" href=\"https://brandelune.github.io/favicon/favicon-72.png\" />
+	<link rel=\"apple-touch-icon\" sizes=\"144x144\" href=\"https://brandelune.github.io/favicon/favicon-144.png\" />
+	<link rel=\"apple-touch-icon\" sizes=\"60x60\" href=\"https://brandelune.github.io/favicon/favicon-60.png\" />
+	<link rel=\"apple-touch-icon\" sizes=\"120x120\" href=\"https://brandelune.github.io/favicon/favicon-120.png\" />
+	<link rel=\"apple-touch-icon\" sizes=\"76x76\" href=\"https://brandelune.github.io/favicon/favicon-76.png\" />
+	<link rel=\"apple-touch-icon\" sizes=\"152x152\" href=\"https://brandelune.github.io/favicon/favicon-152.png\" />
+	<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"https://brandelune.github.io/favicon/favicon-180.png\" />
+
         <link rel=\"stylesheet\" type=\"text/css\" href=\"%2$s\" class=\"baseCSS\"/>
         <link rel=\"stylesheet\" type=\"text/css\" href=\"%3$s\" class=\"dailyCSS\"/>
         <meta charset=\"UTF-8\" />
